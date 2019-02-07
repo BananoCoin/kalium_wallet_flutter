07adde96feb8ebc34020457d3bf4316e12204633
Brandon Berhent <bbedward@gmail.com>
2019-02-07 14:54:26 -0500

    Fix re-connect loop when opening app from notification

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -55,6 +55,7 @@ class _AppHomePageState extends State<AppHomePage>
   AnimationController _placeholderCardAnimationController;
   Animation<double> _opacityAnimation;
   bool _animationDisposed;
+  bool _blockDisconnect = false;
 
   // Receive card instance
   AppReceiveSheet receive;
@@ -119,10 +120,10 @@ class _AppHomePageState extends State<AppHomePage>
         print("onMessage: $message");
       },
       onLaunch: (Map<String, dynamic> message) async {
-        print("onLaunch: $message");
+        _blockDisconnect = true;
       },
       onResume: (Map<String, dynamic> message) async {
-        print("onResume: $message");
+        _blockDisconnect = true;
       },
     );
     _firebaseMessaging.requestNotificationPermissions(
@@ -293,8 +294,12 @@ class _AppHomePageState extends State<AppHomePage>
     // terminate it to be eco-friendly
     switch (state) {
       case AppLifecycleState.paused:
-        setAppLockEvent();
-        StateContainer.of(context).disconnect();
+        if (_blockDisconnect) {
+          _blockDisconnect = false;
+        } else {
+          setAppLockEvent();
+          StateContainer.of(context).disconnect();
+        }
         super.didChangeAppLifecycleState(state);
         break;
       case AppLifecycleState.resumed:
