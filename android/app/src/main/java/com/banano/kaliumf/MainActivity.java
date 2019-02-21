19d602d36cb352d1bb8a661c03e70e4d5a28e5c5
Brandon Berhent <bbedward@gmail.com>
2019-02-21 10:33:47 -0500

    Add code to migrate seed and contacts from legacy android app

--- android/app/src/main/java/com/banano/kaliumf/MainActivity.java
+++ android/app/src/main/java/com/banano/kaliumf/MainActivity.java
@@ -1,13 +1,53 @@
 package com.banano.kaliumf;
 
 import android.os.Bundle;
+import android.util.Base64;
+
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+  private static final String CHANNEL = "fappchannel";
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    Vault.initializeVault(this);
+    generateEncryptionKey();
+
     GeneratedPluginRegistrant.registerWith(this);
+
+      new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+          new MethodChannel.MethodCallHandler() {
+              @Override
+              public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+                  if (call.method.equals("getLegacySeed")) {
+                      result.success(new MigrationStuff().getLegacySeed());
+                  } else if (call.method.equals("getLegacyContacts")) {
+                      result.success(new MigrationStuff().getLegacyContactsAsJson());
+                  } else if (call.method.equals("clearLegacyData")) {
+                      new MigrationStuff().clearLegacyData();
+                  } else {
+                      result.notImplemented();
+                  }
+              }
+          }
+     );
+  }
+
+  /**
+   * generate an encryption key and store in the vault
+   */
+  private void generateEncryptionKey() {
+    if (Vault.getVault().getString(Vault.ENCRYPTION_KEY_NAME, null) == null) {
+      Vault.getVault()
+              .edit()
+              .putString(Vault.ENCRYPTION_KEY_NAME,
+                      Base64.encodeToString(Vault.generateKey(), Base64.DEFAULT))
+              .apply();
+    }
   }
 }
---
+++ android/app/src/main/java/com/banano/kaliumf/MigrationStuff.java
@@ -0,0 +1,39 @@
+package com.banano.kaliumf;
+
+import org.json.JSONArray;
+
+import java.util.List;
+
+import io.realm.Realm;
+
+public class MigrationStuff {
+
+    // Get seed from legacy android app
+    public String getLegacySeed() {
+        Realm realm = new RealmUtil().getRealmInstance();
+        Credentials credentials = realm.where(Credentials.class).findFirst();
+        if (credentials != null) {
+            return credentials.getSeed();
+        }
+        return null;
+    }
+
+    // Get contacts list from legacy android app as serialized json
+    public String getLegacyContactsAsJson() {
+        Realm realm = new RealmUtil().getRealmInstance();
+        List<Contact> contacts = realm.where(Contact.class).findAll();
+        if (contacts.size() == 0) {
+            return null;
+        }
+        JSONArray contactJson = new JSONArray();
+        for (Contact c : contacts) {
+            contactJson.put(c.getJson());
+        }
+        return contactJson.toString();
+    }
+
+    public void clearLegacyData() {
+        Realm realm = new RealmUtil().getRealmInstance();
+        realm.deleteAll();
+    }
+}
