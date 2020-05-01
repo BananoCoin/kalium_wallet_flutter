04a9401d8134dfaf0d51723e7f13070f27b9d9da
Brandon Berhent <bbedward@gmail.com>
2020-05-01 11:48:32 -0400

    Add github actions and fastlane files

---
+++ android/fastlane/flutter_build.sh
@@ -0,0 +1,19 @@
+  
+#!/bin/bash
+
+cd ../../
+if [ "$1" == "--clean" ]
+then
+   echo "Running clean..."
+   flutter clean
+else
+   echo "Skipping clean..."
+fi
+if [ "$1" == "--apk" ]
+then
+   echo "Building APK..."
+   flutter build apk --release
+else
+   echo "Building AAB..."
+   flutter build appbundle --release
+fi
